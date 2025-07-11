45396467451bac07ba6abf466377a5cec114bc1a
Bober <s-daew@ya.ru>
2025-07-11 20:03:22 +0300

    fix errors

--- WeatherProducer/src/main/java/ru/t1/daev/WeatherProducerApplication.java
+++ WeatherProducer/src/main/java/ru/t1/daev/WeatherProducerApplication.java
@@ -2,12 +2,14 @@ package ru.t1.daev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Класс запуска приложения
  * @author Daev
  */
 @SpringBootApplication
+@EnableScheduling
 public class WeatherProducerApplication {
     /**
      * Запуск приложения
