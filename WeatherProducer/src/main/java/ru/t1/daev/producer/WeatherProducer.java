b2ef305737ae007c05b7f170ed90f4dbd83faa2d
Bober <s-daew@ya.ru>
2025-07-11 17:47:23 +0300

    adding a tests for producer

--- WeatherProducer/src/main/java/ru/t1/daev/producer/WeatherProducer.java
+++ WeatherProducer/src/main/java/ru/t1/daev/producer/WeatherProducer.java
@@ -3,6 +3,7 @@ package ru.t1.daev.producer;
 import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -27,7 +28,7 @@ public class WeatherProducer {
      *
      * @param topicName имя топика для отправки сообщений
      */
-    @Setter
+    @Value("${producer.topic.name}")
     private String topicName;
 
     /**
