b1d04de4308f0bb479293164ea0db68b5efe888b
Bober <s-daew@ya.ru>
2025-07-11 15:35:19 +0300

    fix errors

--- WeatherConsumer/src/main/java/ru/t1/daev/WeatherConsumerApplication.java
+++ WeatherConsumer/src/main/java/ru/t1/daev/WeatherConsumerApplication.java
@@ -2,9 +2,17 @@ package ru.t1.daev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * Основной класс приложения для запуска Spring Boot.
+ *
+ * @author Alexandr Daev
+ */
 @SpringBootApplication
 public class WeatherConsumerApplication {
+    private WeatherConsumerApplication() {}
+
     public static void main(String[] args) {
         SpringApplication.run(WeatherConsumerApplication.class, args);
     }
